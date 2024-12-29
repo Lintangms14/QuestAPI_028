7edc78afd7934071e1e7e4cd406565f465e6cd5a
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-29 21:41:38 +0700

    untuk mengelola logika UI dan proses penyisipan data mahasiswa ke dalam database

--- app/src/main/java/com/example/questapi/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/questapi/ui/viewmodel/InsertViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.questapi.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi.Repository.MahasiswaRepository
 import com.example.questapi.model.Mahasiswa
+import kotlinx.coroutines.launch
 
 data class InsertUiEvent(
     val nim: String = "",
@@ -36,3 +43,23 @@ fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
+
+class InsertViewModel(
+    private val mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = InsertUiEvent())
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
