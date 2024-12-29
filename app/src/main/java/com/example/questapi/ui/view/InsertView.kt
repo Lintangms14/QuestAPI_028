f07c7d95e16f5ded28a5374c7ef18cd452fddcb9
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-29 23:09:21 +0700

    untuk mendefinisikan rute dan judul untuk halaman Entry Mahasiswa

---
+++ app/src/main/java/com/example/questapi/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.questapi.ui.view
+
+import com.example.questapi.ui.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
