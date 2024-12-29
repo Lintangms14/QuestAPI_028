87b7e1b237f902494969c3303a675287159a8a8d
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-29 23:00:07 +0700

    untuk menyediakan dan menginisialisasi ViewModel

--- app/src/main/java/com/example/questapi/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi/ui/viewmodel/PenyediaViewModel.kt
@@ -1,8 +1,18 @@
 package com.example.questapi.ui.viewmodel
 
+import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi.MahasiswaApplications
 
 fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiKontak().container.kontakRepository) }
+        initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
+    }
+}
