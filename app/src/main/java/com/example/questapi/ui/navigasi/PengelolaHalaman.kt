387a5a87eb5bc47177b083864f83b1e55d7f26b7
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-30 01:07:09 +0700

    untuk mengelola navigasi antar dua layar utama dalam aplikasi

---
+++ app/src/main/java/com/example/questapi/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,39 @@
+package com.example.questapi.ui.navigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questapi.ui.view.DestinasiEntry
+import com.example.questapi.ui.view.DestinasiHome
+import com.example.questapi.ui.view.EntryMhsScreen
+import com.example.questapi.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToltemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
