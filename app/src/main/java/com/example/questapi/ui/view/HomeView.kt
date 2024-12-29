22d3e25e3c7358b02ef6ea534f21e7e2ef807244
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-29 23:45:10 +0700

    sebuah objek yang mengimplementasikan interface DestinasiNavigasi

---
+++ app/src/main/java/com/example/questapi/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.questapi.ui.view
+
+import com.example.questapi.ui.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
