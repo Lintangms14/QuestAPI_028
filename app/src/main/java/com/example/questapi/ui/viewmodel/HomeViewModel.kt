76ccde3fb1456db11d6d2e3391878926033907e9
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-29 22:00:23 +0700

    untuk mengelola data mahasiswa dan memperbarui status UI sesuai dengan hasil operasi

--- app/src/main/java/com/example/questapi/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi/ui/viewmodel/HomeViewModel.kt
@@ -1,9 +1,53 @@
 package com.example.questapi.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.questapi.Repository.MahasiswaRepository
 import com.example.questapi.model.Mahasiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeUiState{
     data class Succes(val mahasiswa: List<Mahasiswa>): HomeUiState()
     object Error: HomeUiState()
     object Loading: HomeUiState()
+}
+
+class HomeViewModel(
+    private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Succes(mhs.getMahasiswa())
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteMhs(nim: String){
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
 }
