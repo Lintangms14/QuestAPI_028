a7edbd7b92ef1741612ac9c1fa47c72cc62bab06
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-30 10:38:49 +0700

    untuk mengelola data dan status UI pada detail mahasiswa berdasarkan NIM

--- app/src/main/java/com/example/questapi/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi/ui/viewmodel/DetailViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.questapi.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.questapi.Repository.MahasiswaRepository
 import com.example.questapi.model.Mahasiswa
+import com.example.questapi.ui.view.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class DetailUiState {
     data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
@@ -8,3 +19,31 @@ sealed class DetailUiState {
     object Loading : DetailUiState()
 }
 
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mhs: MahasiswaRepository
+) : ViewModel() {
+
+    var mahasiswaDetailState: DetailUiState by mutableStateOf(DetailUiState.Loading)
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    init {
+        getMahasiswabyNim()
+    }
+
+    fun getMahasiswabyNim() {
+        viewModelScope.launch {
+            mahasiswaDetailState = DetailUiState.Loading
+            mahasiswaDetailState = try {
+                val mahasiswa = mhs.getMahasiswabyNim(_nim)
+                DetailUiState.Success(mahasiswa)
+            } catch (e: IOException) {
+                DetailUiState.Error
+            } catch (e: HttpException) {
+                DetailUiState.Error
+            }
+        }
+    }
+}
