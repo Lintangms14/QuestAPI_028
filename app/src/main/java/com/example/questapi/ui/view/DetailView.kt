f115c785e49d4cc07daabf27a32673f2dda9d378
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-30 10:43:29 +0700

    untuk menampilkan detail informasi mahasiswa

--- app/src/main/java/com/example/questapi/ui/view/DetailView.kt
+++ app/src/main/java/com/example/questapi/ui/view/DetailView.kt
@@ -1,5 +1,14 @@
 package com.example.questapi.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.questapi.ui.navigasi.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi {
@@ -7,4 +16,29 @@ object DestinasiDetail: DestinasiNavigasi {
     override val titleRes = "Detail Data Mahasiswa"
     const val NIM = "nim"
     val routesWithArg = "$route/{$NIM}"
+}
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = judul,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = isinya,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    }
 }
