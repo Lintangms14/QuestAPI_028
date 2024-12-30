f351dce972dab91ff405b7c4306666ea80d84f75
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-30 10:48:10 +0700

    untuk menampilkan informasi detail mahasiswa dalam bentuk card

--- app/src/main/java/com/example/questapi/ui/view/DetailView.kt
+++ app/src/main/java/com/example/questapi/ui/view/DetailView.kt
@@ -2,13 +2,19 @@ package com.example.questapi.ui.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.questapi.model.Mahasiswa
 import com.example.questapi.ui.navigasi.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi {
@@ -16,6 +22,34 @@ object DestinasiDetail: DestinasiNavigasi {
     override val titleRes = "Detail Data Mahasiswa"
     const val NIM = "nim"
     val routesWithArg = "$route/{$NIM}"
+}
+
+@Composable
+fun ItemDetailMhs(
+    modifier: Modifier = Modifier,
+    mahasiswa: Mahasiswa
+) {
+    Card(
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+            ComponentDetailMhs(judul = "Nama", isinya = mahasiswa.nama)
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+            ComponentDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+            ComponentDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+            ComponentDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+            ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        }
+    }
 }
 
 @Composable
